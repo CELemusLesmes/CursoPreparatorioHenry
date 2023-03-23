ccda80aafce88fa786e8fda2efe28a52010943a7
CELemusLesmes <krafcarlos@gmail.com>
2023-03-22 22:51:48 -0500

    archivos principales del repaso para programacióm basica con javaScript

---
+++ operadores.js
@@ -0,0 +1,21 @@
+
+/*//Suma
+console.log(3 + 5);
+
+//Resta
+console.log(14 - 7);
+
+//Multiplicacion
+console.log(8 * 6);
+
+//Division
+console.log(30 / 9);
+
+//Resto
+console.log(95 % 4);
+
+//Prueba de precedencia
+console.log(34 + 20 / (2 - 8));
+*/
+ var nombre = 'carlos'
+console.log( nombre +' ' +'lemus'+' '+'lesmes');
---
+++ variables.js
@@ -0,0 +1,25 @@
+
+
+//string
+var bootcam = 'Henry';
+
+//Numeros
+
+var numeroEntero  = 17;
+var  numeroNegativo = -4;
+var numeroDecimal=11.75;
+
+//Booleanos 
+
+var estoyEntendiendo = true;
+var estoyAburrido = false;
+
+//Undefined
+
+var cajon1; // cunado no  esta defenido el valor 
+
+//Null
+
+var sinNada  = Null // cuando el desarrollador le un valor indefindo 
+
+
